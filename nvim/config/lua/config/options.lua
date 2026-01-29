de983d7e03de7c6653593d78996d701d1d955afa
Maximiliano Florentin <macsee13@gmail.com>
2026-01-29 15:13:32 -0300

    yn mbp m3 configs

--- nvim/config/lua/config/options.lua
+++ nvim/config/lua/config/options.lua
@@ -2,6 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
-vim.opt.mouse = "a"
-vim.opt.clipboard = "unnamed"
+vim.opt.mouse = "r"
+vim.opt.clipboard = "unnamedplus"
 vim.opt.cursorline = true
